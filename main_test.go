3ae15705fc7e8c034c934c0b8ce7334c5e787be1
Abhishek Chanda <abhishek.chanda@emc.com>
2015-10-08 15:48:41 -0700

    Add code

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+        "testing"
+)
+
+func TestGet(t *testing.T) {
+        got, err := Get("net.ipv4.tcp_sack")
+        ex := "1"
+        if err != nil {
+                t.Fatalf("Could not read key")
+        }
+        if got != ex {
+                t.Errorf("Expected: %s, got %s", ex, got)
+        }
+}
+
+func TestSet(t *testing.T) {
+        ex := "0"
+        err := Set("net.ipv4.ip_forward", ex)
+        if err != nil {
+                t.Fatalf("err")
+        }
+        got, err := Get("net.ipv4.ip_forward")
+        if err != nil {
+                t.Fatalf("Could not read key")
+        }
+        if got != ex {
+                t.Errorf("Expected: %s, got %s", ex, got)
+        }
+}
